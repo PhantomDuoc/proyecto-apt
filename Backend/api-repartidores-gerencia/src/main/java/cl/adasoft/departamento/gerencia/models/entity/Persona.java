482137da41b9011b49f2a7c61ef3290eca40bdc8
Hugo Cerda Jara <hcerdaja@redhat.com>
2022-05-23 22:11:57 -0400

    commit

---
+++ Backend/api-repartidores-gerencia/src/main/java/cl/adasoft/departamento/gerencia/models/entity/Persona.java
@@ -0,0 +1,55 @@
+package cl.adasoft.departamento.gerencia.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+
+@Data 
+@Entity
+@Table(name="users")
+public class Persona {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 10, max = 100, message = "The name cannot exceed 100 characters")
+	@Column(name="username")
+	private String username;
+
+	@NotBlank
+	@Column(name="password")
+	private String password;
+	
+	@NotBlank
+	@Column(name="type")
+	private Long type;
+
+	@NotBlank
+	@Column(name="rut")
+	private Long rut;
+
+	@NotBlank
+	@Column(name="direccion")
+	private String direccion;
+	
+	@NotBlank
+	@Column(name="email")
+	@Email
+	private String email;
+
+	@NotBlank
+	@Column(name="telefono")
+	private Long telefono;
+	
+}
---
+++ Backend/api-usuarios-gerencia/src/main/java/cl/adasoft/departamento/gerencia/models/dao/PersonaDao.java
@@ -0,0 +1,15 @@
+package cl.adasoft.departamento.gerencia.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import cl.adasoft.departamento.gerencia.models.entity.Persona;
+
+public interface PersonaDao extends PagingAndSortingRepository<Persona,Long> {
+	
+	  @Query("select p from Persona p where p.rut=?1")
+	  public Persona findByRut(Long rut);
+
+	  
+	  
+}
