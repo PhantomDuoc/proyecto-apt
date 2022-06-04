cce17a8b684e3da2b7599dc8341eccc3a1fc29f6
felipeandres97 <felipecof97@hotmail.com>
2022-06-04 16:21:50 -0400

    subida app web

--- Web-Django/sushi index completo, falta ubicacion y meter carrito de compra  y rest api/sushi index completo/sushi/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Usuarios,Repartidores,Pedidos
-# Register your models here.
-
-#Permite administrar el modelo completo 
-
-admin.site.register(Usuarios)
-admin.site.register(Repartidores)
-admin.site.register(Pedidos)
