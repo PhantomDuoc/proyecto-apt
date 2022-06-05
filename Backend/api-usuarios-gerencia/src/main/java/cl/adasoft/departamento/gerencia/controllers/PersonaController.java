81052f9db166b0bb35ecc5379cf056babf101940
Hugo Cerdá <hu.cerdaj@duocuc.cl>
2022-06-05 18:14:02 -0400

    avances =)

--- Backend/api-usuarios-gerencia/src/main/java/cl/adasoft/departamento/gerencia/controllers/PersonaController.java
+++ Backend/api-usuarios-gerencia/src/main/java/cl/adasoft/departamento/gerencia/controllers/PersonaController.java
@@ -9,9 +9,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,37 +26,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import io.swagger.v3.oas.annotations.Operation;
 
-@RestController
+@CrossOrigin(origins = "*")   //para permitir el acceso a la api desde cualquier origen
+@RestController  //para que sea un controlador rest
 public class PersonaController {
 	
 	@Autowired 
-	private IPersonaService participantService;
+	private IPersonaService participantService;  //para inyectar el servicio
 	
-	@GetMapping("/findAll")
-	@Operation(summary = "Get users by name", description = "Returns the users filtered by name")
-	public Page<Persona> findAll(@RequestParam(name="page", defaultValue="0") int page){
+	@GetMapping("/findAll")  //para indicarle a spring que es una peticion get
+	@Operation(summary = "Get users by name", description = "Returns the users filtered by name")  //para indicarle a swagger que es una operacion get
+	public Page<Persona> findAll(@RequestParam(name="page", defaultValue="0") int page){  //para indicarle a spring que es una peticion get
 		
-		Pageable pageRequest =  PageRequest.of(page, 5);
-		Page<Persona> participants = participantService.findAll(pageRequest);
-		return participants;
+		Pageable pageRequest =  PageRequest.of(page, 5);  
+		Page<Persona> participants = participantService.findAll(pageRequest);  //para obtener todos los participantes
+		return participants;  //para retornar los participantes
 		
 	}
 	
-	@PostMapping("/create")
-	public ResponseEntity<Persona>  save(@RequestBody Persona persona) {
+	@PostMapping("/create")  //para indicarle a spring que es una peticion post
+	public ResponseEntity<Persona>  save(@RequestBody Persona persona) {  
 		
-		return new ResponseEntity<>(participantService.save(persona),HttpStatus.CREATED);
+		return new ResponseEntity<>(participantService.save(persona),HttpStatus.CREATED);  //para retornar el objeto persona y el codigo de estado
 	}
 	
 	
-	@DeleteMapping("/delete/{id}")
-	public void  update(@PathVariable Long id) {
+	@DeleteMapping("/delete/{id}")  //para indicarle a spring que es una peticion delete
+	public void  update(@PathVariable Long id) {  
 		
-		participantService.delete(id);
+		participantService.delete(id);  //para eliminar el objeto persona
 	}	
 	
-/* 	Reemplazar por buscar por nombre de usuario
- */	@GetMapping("/findByRut/{rut}")
+	@GetMapping("/findByRut/{rut}")	 	
 	public ResponseEntity<Persona> findByRut(@PathVariable Long rut){
 		Persona response = participantService.findByRut(rut);
 		
@@ -76,5 +78,23 @@ public class PersonaController {
 		
 		return response;
     }
+
+	@GetMapping(/*"/findByName/{name}"*/)
+	public Page<Persona> findByName(@RequestParam(name="page", defaultValue="0") int page, @RequestParam(name="name", defaultValue="") String name){
+		Pageable pageRequest =  PageRequest.of(page, 5);  
+		Page<Persona> participants = participantService.findByName(pageRequest, name);
+		return participants;
+	}
+
+	@PutMapping("/update")
+	public ResponseEntity<Persona> update(@RequestBody Persona persona){
+		return new ResponseEntity<>(participantService.save(persona),HttpStatus.CREATED);
+	}
+
+	@Operation
+	@DeleteMapping("/delete/{id}")
+	public void deleteById(@PathVariable Long id){  
+		participantService.delete(id);
+	}
 	
 }
--- Backend/api-usuarios-gerencia/src/main/java/cl/adasoft/departamento/gerencia/models/services/PersonaServiceImp.java
+++ Backend/api-usuarios-gerencia/src/main/java/cl/adasoft/departamento/gerencia/models/services/PersonaServiceImp.java
@@ -57,4 +57,42 @@ public class PersonaServiceImp implements IPersonaService {
 		return participantsDao.findById(rut);
 	}	
 	
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Persona> findByName(Pageable pageRequest, String name) {
+		
+		return participantsDao.findByNombreContaining(pageRequest, name);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Persona> findByLastName(Pageable pageRequest, String lastName) {
+		
+		return participantsDao.findByApellidoContaining(pageRequest, lastName);
+	}
+
+	@Override
+	public Page<Persona> findByRut(Pageable pageRequest, Long rut) {
+		// TODO Auto-generated method stub
+
+		return participantsDao.findByRut(pageRequest, rut);
+	}
+
+	@Override
+	public Page<Persona> findByEmail(Pageable pageRequest, String email) {
+		// TODO Auto-generated method stub
+		return participantsDao.findByEmail(pageRequest, email);
+	}
+
+	@Override
+	public Page<Persona> findByPhone(Pageable pageRequest, String phone) {
+		// TODO Auto-generated method stub
+		return participantsDao.findByPhone(pageRequest, phone);
+	}
+
+	@Override
+	public Page<Persona> findByAddress(Pageable pageRequest, String address) {
+		// TODO Auto-generated method stub
+		return participantsDao.findByAddress(pageRequest, address);
+	}
 }
