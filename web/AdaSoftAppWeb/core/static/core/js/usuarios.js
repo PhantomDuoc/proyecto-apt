e1feaead732a2c0b8af5f651c0781c6df96c13c9
Hugo Cerdá <hu.cerdaj@duocuc.cl>
2022-06-15 12:25:01 -0400

    funcionando delete dinamico, arreglar conectar usuarios.js

--- web/AdaSoftAppWeb/core/static/core/js/usuarios.js
+++ web/AdaSoftAppWeb/core/static/core/js/usuarios.js
@@ -1,1 +1,76 @@
-let url = "http://localhost:8091/v1/departamento/gerencia/usuario/findAll"
+fetch('http://localhost:8091/v1/departamento/gerencia/usuario/update', {
+        method: "PUT",
+        headers: {
+            'Content-type': 'application/json',
+            'Allow-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            id: 2,
+            username: "admin",
+            password: "admin",
+            rut: "12345678-9",
+            direccion: "direccion",
+            email: "email",
+        })
+    })
+    .then(res => {
+        if (res.ok) {
+            console.log("HTTP request successful")
+        } else {
+            console.log("HTTP request unsuccessful")
+        }
+        return res
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+function updateUser() {
+    fetch('http://localhost:8091/v1/departamento/gerencia/usuario/update', {
+            method: "PUT",
+            headers: {
+                'Content-type': 'application/json',
+                'Allow-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                id: 2,
+                username: "admin",
+                password: "admin",
+                rut: "12345678-9",
+                direccion: "direccion",
+                email: "email",
+            })
+        })
+        .then(res => {
+            if (res.ok) {
+                console.log("HTTP request successful")
+            } else {
+                console.log("HTTP request unsuccessful")
+            }
+            return res
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+}
+
+function deleteUser() {
+    fetch('http://localhost:8091/v1/departamento/gerencia/usuario/delete/5', {
+            method: "DELETE",
+            headers: {
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        .then(res => {
+            if (res.ok) {
+                console.log("HTTP request successful")
+            } else {
+                console.log("HTTP request unsuccessful")
+            }
+            return res
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+}
