72f57cfc1c9d98fb0cc8ffdc8ddcc1cf109240d4
felipeandres97 <felipecof97@hotmail.com>
2022-06-18 06:41:40 -0400

    .

--- web/VeterinariaWeb/adopciones/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import EstadoAdopcion, enAdopcion
-
-# Register your models here.
-admin.site.register(EstadoAdopcion)
-admin.site.register(enAdopcion)
