e74af1ae85c5e5cc65901d3dbdf37e50f79ee596
felipeandres97 <felipecof97@hotmail.com>
2022-06-18 06:44:51 -0400

    .

--- web/AdaSoftAppWeb/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuarios,Pedidos
-# Register your models here.
-
-#Permite administrar el modelo completo 
-
-admin.site.register(Usuarios)
-admin.site.register(Pedidos)
