676f2173c95b1670578f4765770af33f13af9afc
Hugo Cerdá <hu.cerdaj@duocuc.cl>
2022-06-18 16:35:43 -0400

    Revert "Hurto."
    This reverts commit 45a9010ff3ae7adaa47f6a84e45770a85205f59d.

---
+++ web/VeterinariaWeb/adopciones/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import EstadoAdopcion, enAdopcion
+
+# Register your models here.
+admin.site.register(EstadoAdopcion)
+admin.site.register(enAdopcion)
---
+++ web/VeterinariaWeb/rest_adopcion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RestAdopcionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rest_adopcion'
