cd8197d8152e50537d175fe3bc978a63c4b46c3f
Hugo Cerdá <hu.cerdaj@duocuc.cl>
2022-06-18 16:43:38 -0400

    Revert "hurto, no se ocupan proyectos ajenos realizados."
    This reverts commit e8e33f81f1b23a1343d7a2f7e535a2795f34c962.

---
+++ web/AdaSoftAppWeb/core/static/core/js/form.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario);
+});
+
+
+function validarFormulario(evento) {
+    evento.preventDefault();
+    var usuario = document.getElementById('user').value;
+    var password = document.getElementById('pass').value;
+    let tipoUser;
+    fetchAsync("http://localhost:8091/v1/departamento/gerencia/usuario/findByUsername/" + usuario).then(function(data) {
+        console.log('aqui');
+        console.log(data.email);
+        console.log(data);
+        if (data.length === 0) {
+            alert('Usuario no existe');
+            return;
+        }
+        if (data.password !== password) {
+            alert('Contraseña Incorrecta');
+            return;
+        }
+        if (data.password === password) {
+            alert('Bienvenido ' + data.username);
+        }
+        if (data.type == 0) {
+            window.location.href = "http://localhost:8000/dashboard";
+        }
+        if (data.type != 0) {
+            window.location.href = "http://localhost:8000/indexlog";
+        }
+    });
+}
+
+async function fetchAsync(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
