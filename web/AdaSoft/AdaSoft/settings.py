c194988d6a036b5cc0987d66ce6d4c89e90c8bc0
Hugo Cerdá <hu.cerdaj@duocuc.cl>
2022-06-18 20:04:53 -0400

    comienzo nueva web app =)

--- web/AdaSoft/AdaSoft/settings.py
+++ web/AdaSoft/AdaSoft/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +67,12 @@ TEMPLATES = [
             ],
         },
     },
+    
 ]
+SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
 
 WSGI_APPLICATION = 'AdaSoft.wsgi.application'
 
--- web/AdaSoft/core/views.py
+++ web/AdaSoft/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def dashboard(request):
+    return render(request, 'core/dashboard.html')
