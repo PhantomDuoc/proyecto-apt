4a38d3d6f839100126d31c1a52396870d3043ec0
Hugo Cerdá <hu.cerdaj@duocuc.cl>
2022-06-27 19:10:11 -0400

    aplicando side menu a todas las page jejeje

--- mobile/AdaSoft/src/app/app-routing.module.ts
+++ mobile/AdaSoft/src/app/app-routing.module.ts
@@ -10,6 +10,30 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./adasoft/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./adasoft/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./adasoft/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'map',
+    loadChildren: () => import('./adasoft/map/map.module').then( m => m.MapPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./adasoft/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'recovery',
+    loadChildren: () => import('./adasoft/recovery/recovery.module').then( m => m.RecoveryPageModule)
   }
 ];
 
--- mobile/AdaSoft/src/app/app.component.ts
+++ mobile/AdaSoft/src/app/app.component.ts
@@ -6,13 +6,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Inicio', url: 'home', icon: 'home' },
+    { title: 'Mapa', url: 'map', icon: 'map' },
+    { title: 'Perfil', url: 'profile', icon: 'person' },
+    { title: 'Registrarse', url: 'register', icon: 'log-out' },
+    { title: 'Iniciar Sesión', url: 'login', icon: 'log-in' },
+    { title: 'Recuperar Contraseña', url: 'recovery', icon: 'key' },
+    { title: 'Cerrar Sesión', url: 'login', icon: 'log-out' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  /*   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders']; */
   constructor() {}
 }
